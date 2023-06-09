7cc29a081733dfbb73074ce8bdebff8c89d49d6f
Adriane Boyd <adrianeboyd@gmail.com>
2023-06-09 17:46:30 +0200

    Also upgrade transformers

--- remote.sh
+++ remote.sh
@@ -47,7 +47,7 @@ export CIBW_SKIP="pp* *-musllinux* *i686*"
 export CIBW_BEFORE_TEST="pip install -r requirements.txt"
 # torch is not always compiled against the oldest support numpy, so upgrade
 # before testing
-export CIBW_TEST_COMMAND="unset PIP_CONSTRAINT && python -m pip install -U numpy && pytest --tb=native --pyargs $MODULE_NAME"
+export CIBW_TEST_COMMAND="unset PIP_CONSTRAINT && python -m pip install -U numpy transformers && pytest --tb=native --pyargs $MODULE_NAME"
 # By default cibuildwheel doesn't strip debug info from libraries:
 #    https://github.com/pypa/cibuildwheel/issues/331
 # But you pretty much always want this for production end-user releases, so we override
