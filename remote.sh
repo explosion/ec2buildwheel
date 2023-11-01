91c33adc1307e0bec4005172bfda7895d875386c
Adriane Boyd <adrianeboyd@gmail.com>
2023-11-01 14:44:08 +0100

    Install all requirements except for spacy-transformers

--- remote.sh
+++ remote.sh
@@ -48,7 +48,11 @@ export CIBW_BUILD_FRONTEND=pip
 export CIBW_SKIP="pp* *-musllinux* *i686* cp312-*"
 # clean cython-generated files between builds to handle profiling
 # settings, since the builds aren't isolated
-export CIBW_BEFORE_BUILD="pip install numpy 'cython<3' && python setup.py clean"
+if [ "$PACKAGE_NAME" = "spacy_transformers" ]; then
+    export CIBW_BEFORE_BUILD="pip install numpy 'cython<3' && python setup.py clean"
+else
+    export CIBW_BEFORE_BUILD="pip install -r requirements.txt && python setup.py clean"
+fi
 # torch is not always compiled against the oldest support numpy, so upgrade
 # before testing
 export CIBW_BEFORE_TEST="unset PIP_CONSTRAINT && pip install -U -r requirements.txt && pip cache purge"
