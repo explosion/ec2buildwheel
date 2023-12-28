da9a6481954671fb54c8be718443d565eae73c13
Adriane Boyd <adrianeboyd@gmail.com>
2023-12-28 11:11:08 +0100

    Remove build constraint handling

--- remote.sh
+++ remote.sh
@@ -39,12 +39,7 @@ python3 -m venv ~/myenv
 export CIBW_BEFORE_ALL="curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain stable"
 export CIBW_ENVIRONMENT='PATH="$PATH:$HOME/.cargo/bin" SPACY_NUM_BUILD_JOBS=8'
 export CIBW_BUILD_VERBOSITY=1
-if [ -e build-constraints.txt ]; then
-    export CIBW_ENVIRONMENT="$CIBW_ENVIRONMENT PIP_CONSTRAINT=build-constraints.txt"
-fi
-# build constraints through PIP_CONSTRAINT only work with pip frontend,
-# with the drawback that the pip builds aren't isolated
-export CIBW_BUILD_FRONTEND=pip
+export CIBW_BUILD_FRONTEND=build
 export CIBW_SKIP="pp* *-musllinux* *i686*"
 # for spacy: clean cython-generated files between builds to handle profiling
 # settings, since the builds aren't isolated; a cleaner/nicer version would
@@ -53,8 +48,8 @@ export CIBW_SKIP="pp* *-musllinux* *i686*"
 if [ "$MODULE_NAME" = "spacy" ]; then
     export CIBW_BEFORE_BUILD="pip install -r requirements.txt && python setup.py clean"
 fi
-export CIBW_BEFORE_TEST="unset PIP_CONSTRAINT && pip install -U -r requirements.txt"
-export CIBW_TEST_COMMAND="unset PIP_CONSTRAINT && pip install 'urllib3<2' && pytest --tb=native --pyargs $MODULE_NAME"
+export CIBW_BEFORE_TEST="pip install -U -r requirements.txt"
+export CIBW_TEST_COMMAND="pip install 'urllib3<2' && pytest --tb=native --pyargs $MODULE_NAME"
 # By default cibuildwheel doesn't strip debug info from libraries:
 #    https://github.com/pypa/cibuildwheel/issues/331
 # But you pretty much always want this for production end-user releases, so we override
